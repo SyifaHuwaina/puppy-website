ac3c63ad1ac2e4dd6e27273d255d07517cc9581c
Syifa <24FTT1093@student.pb.edu.bn>
2026-06-10 16:25:29 +0800

    Add OpenWhen page component (placeholder)

---
+++ src/pages/openwhen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function OpenWhen() {
+	return (
+		<main className="openwhen-page">
+			<section className="openwhen-panel">
+				<p className="page-tag">for my sweetest puppy</p>
+				<h1>📬 Open When</h1>
+				<p>This is a little collection of messages for when you need one.</p>
+			</section>
+		</main>
+	)
+}
+
+export default OpenWhen
